bbe76cf3d4002b2d59ad086f329251f9bf71d618
dodo4114 <52357676+dodo4114@users.noreply.github.com>
2020-06-19 00:33:06 +0900

    Recipe Timer Development Environment Initialized

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -1,48 +1,53 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import * as React from 'react';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import * as React from "react";
 
-import TabBarIcon from '../components/TabBarIcon';
-import HomeScreen from '../screens/HomeScreen';
-import LinksScreen from '../screens/LinksScreen';
+import TabBarIcon from "../components/TabBarIcon";
+import HomeScreen from "../screens/HomeScreen";
+import LinksScreen from "../screens/LinksScreen";
 
 const BottomTab = createBottomTabNavigator();
-const INITIAL_ROUTE_NAME = 'Home';
+const INITIAL_ROUTE_NAME = "Home";
 
 export default function BottomTabNavigator({ navigation, route }) {
-  // Set the header title on the parent stack navigator depending on the
-  // currently active tab. Learn more in the documentation:
-  // https://reactnavigation.org/docs/en/screen-options-resolution.html
-  navigation.setOptions({ headerTitle: getHeaderTitle(route) });
+	// Set the header title on the parent stack navigator depending on the
+	// currently active tab. Learn more in the documentation:
+	// https://reactnavigation.org/docs/en/screen-options-resolution.html
+	navigation.setOptions({ headerTitle: getHeaderTitle(route) });
 
-  return (
-    <BottomTab.Navigator initialRouteName={INITIAL_ROUTE_NAME}>
-      <BottomTab.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{
-          title: 'Get Started',
-          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-code-working" />,
-        }}
-      />
-      <BottomTab.Screen
-        name="Links"
-        component={LinksScreen}
-        options={{
-          title: 'Resources',
-          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-book" />,
-        }}
-      />
-    </BottomTab.Navigator>
-  );
+	return (
+		<BottomTab.Navigator initialRouteName={INITIAL_ROUTE_NAME}>
+			<BottomTab.Screen
+				name="Recipe"
+				component={HomeScreen}
+				options={{
+					title: "나의 레시피",
+					tabBarIcon: ({ focused }) => (
+						<TabBarIcon focused={focused} name="book" />
+					),
+				}}
+			/>
+			<BottomTab.Screen
+				name="FindRecipe"
+				component={LinksScreen}
+				options={{
+					title: "레시피 찾기",
+					tabBarIcon: ({ focused }) => (
+						<TabBarIcon focused={focused} name="magnify" />
+					),
+				}}
+			/>
+		</BottomTab.Navigator>
+	);
 }
 
 function getHeaderTitle(route) {
-  const routeName = route.state?.routes[route.state.index]?.name ?? INITIAL_ROUTE_NAME;
+	const routeName =
+		route.state?.routes[route.state.index]?.name ?? INITIAL_ROUTE_NAME;
 
-  switch (routeName) {
-    case 'Home':
-      return 'How to get started';
-    case 'Links':
-      return 'Links to learn more';
-  }
+	switch (routeName) {
+		case "Recipe":
+			return "레시피 타이머";
+		case "FindRecipe":
+			return "레시피 타이머";
+	}
 }
